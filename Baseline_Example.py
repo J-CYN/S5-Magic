2552e393b282ce9d1eb346317362ca91418b795f
J-CYN <jabudiab2024@gmail.com>
2025-09-24 01:05:34 -0400

    Added a few resources to checkout in Baseline_Example.py, modified the input_screen.html by creating a form-group class to better organize inputs and labels associated with querying the database.

--- Baseline_Example.py
+++ Baseline_Example.py
@@ -1,3 +1,6 @@
-import sqlite3
-#flask or
-#django
+import sqlite3 #look into SQLITE WAL apparently #https://docs.python.org/3/library/sqlite3.html
+con=sqlite3.connect()
+cur=con.cursor()
+cur.execute("")
+con.close()
+#flask DEF
