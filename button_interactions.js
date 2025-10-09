105b2fe745c9083a61378d1b52aac830186bb368
J-CYN <jabudiab2024@gmail.com>
2025-10-09 18:23:10 -0400

    Minor edits and initialization of the Javascript document

--- button_interactions.js
+++ button_interactions.js
@@ -1,1 +1,11 @@
-//function buttonPressed()
+const buttonGrid = document.getElementById("button-grid");
+
+buttonGrid.addEventListener("click", function(event){
+    if (event.target.tagName == "BUTTON"){
+        button_pressed(event.target);
+    }
+});
+
+function button_pressed(button){
+    button.style.backgroundColor = "#5f4505";
+}
